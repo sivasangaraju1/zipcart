89bbd8f4c7dd5ba501431a6e0e30591700b0711e
sivasangaraju1 <sivasangaraju1@gmail.com>
2026-01-02 17:47:01 +0530

    Updated supabase.ts

--- mobile/customer-app/src/lib/supabase.ts
+++ mobile/customer-app/src/lib/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = 'https://hbtzwdeqzfrjdlegemcs.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhidHp3ZGVxemZyamRsZWdlbWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNDIwMTQsImV4cCI6MjA4MjkxODAxNH0.7u0c8RuWRvHIKEEnbo_zxRW1FQ0800Lgb0mRFe0lYZU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
